66a0143f2944fcc07b2a0f287eba04614aa00106
Scott Williams <williamsjs1224@gmail.com>
2015-07-22 11:47:13 -0400

    add styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :movies
   has_many :beers, through: :movies
 
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true
+  validates :email, :password, :name, presence: true
 
 end
