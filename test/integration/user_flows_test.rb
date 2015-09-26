6faaa24e845f0bf0c88291341d8d819486bc3817
Scott Williams <williamsjs1224@gmail.com>
2015-09-26 19:55:03 -0400

    begin integration test for login

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
-class UserFlowsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class LoginTest < ActionDispatch::IntegrationTest
+
+  test "login to site" do
+    get sessions_login_path
+    assert_select "input[type='password']", 2 # 1 for create modal and 1 for login
+    post sessions_login_path, password: "password", email: "test@test.com"
+    assert_response :success
+  end
+
+  
 end
